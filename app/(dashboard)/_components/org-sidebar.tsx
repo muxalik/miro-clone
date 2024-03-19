31016f2a6322c0b88915ceda91b292d481d3b1c6
Mihail Kornilov <105970406+muxalik@users.noreply.github.com>
2024-03-19 20:37:31 +0300

    feature dashboard layout

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+const OrgSidebar = () => {
+  return (
+    <div className='hidden lg:flex flex-col px-5 w-[206px] space-y-6 bg-red-500'>
+      Org Sidebar
+    </div>
+  )
+}
+
+export default OrgSidebar
