f9ca9010c8d92e3c4389abd005721e6f717e9fe6
Mihail Kornilov <105970406+muxalik@users.noreply.github.com>
2024-03-21 07:01:00 +0300

    feature create board

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,32 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
+import Icons from '@/components/ui/icons'
+import { api } from '@/convex/_generated/api'
+import { useOrganization } from '@clerk/nextjs'
+import { useMutation } from 'convex/react'
 import Image from 'next/image'
+import { useTransition } from 'react'
+import { toast } from 'sonner'
 
 const EmptyBoards = () => {
+  const [isPending, startTransition] = useTransition()
+  const create = useMutation(api.board.create)
+  const { organization } = useOrganization()
+
+  const onClick = () => {
+    if (!organization) return null
+
+    startTransition(() => {
+      create({
+        title: 'Untitled',
+        orgId: organization.id,
+      })
+        .then(() => toast.success('Board created'))
+        .catch(() => toast.error('Failed to create board'))
+    })
+  }
+
   return (
     <div className='h-full w-full flex flex-col items-center justify-center'>
       <Image src='/note.svg' width={110} height={110} alt='Empty' />
@@ -10,7 +35,10 @@ const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className='mt-6'>
-        <Button size='lg'>Create board</Button>
+        <Button onClick={onClick} size='lg' disabled={isPending}>
+          {isPending && <Icons.spinner className='animate-spin mr-2 h-6 w-6' />}
+          <span>{isPending ? 'Creating...' : 'Create board'}</span>
+        </Button>
       </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ClerkClientProvider from '@/providers/convex-client-provider'
+import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <ClerkClientProvider>{children}</ClerkClientProvider>
+        <ClerkClientProvider>
+          <Toaster />
+          {children}
+        </ClerkClientProvider>
       </body>
     </html>
   )
